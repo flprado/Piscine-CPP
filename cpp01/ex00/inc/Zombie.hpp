47ea156f8ab275b1146ccae53c4eca02cba3e2a0
Florian Prado <fprado@z4r9p5.42lyon.fr>
2023-01-26 17:24:37 +0100

    2601

--- cpp01/ex00/inc/Zombie.hpp
+++ cpp01/ex00/inc/Zombie.hpp
@@ -1,14 +1,27 @@
-#include "general.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fprado <fprado@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 16:34:28 by fprado            #+#    #+#             */
+/*   Updated: 2023/01/26 16:50:55 by fprado           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+# include <iostream>
+# include <string>
 
 class Zombie
 {
     private:
         std::string name;
 
-    public:
-        Zombie(std::string name);
+    public: 
         Zombie(void);
+        Zombie(std::string name);
         ~Zombie(void);
-
         void announce(void);
 };
--- cpp01/ex00/src/zombie.cpp
+++ cpp01/ex00/src/zombie.cpp
@@ -1,28 +1,47 @@
-#include "general.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fprado <fprado@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 16:34:15 by fprado            #+#    #+#             */
+/*   Updated: 2023/01/26 16:50:22 by fprado           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Zombie.hpp"
 
 void    randomChump(std::string name)
 {
-    Zombie zombie = Zombie(name);
-    zombie.announce();
+	Zombie zombie = Zombie(name);
+	zombie.announce();
 }
 
 Zombie *newZombie(std::string name)
 {
-    Zombie *zombie = new Zombie(name);
-    return (zombie);
+	Zombie *zombie = new Zombie(name);
+	return (zombie);
+}
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+	return ;
 }
 
 Zombie::Zombie(void)
 {
-    return ;
+	return ;
 }
 
-~Zombie::Zombie(void)
+Zombie::~Zombie(void)
 {
-    return ;
+	return ;
 }
 
 void    Zombie::announce(void)
 {
-    std::cout << this.name <<"Braiiiiiiinnnssss..." << std::endl;
+	std::cout << this->name <<"BraiiiiiiinnnzzzZ..." << std::endl;
 }
